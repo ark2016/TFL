5a0f683e7c9484d9021b60a915cba6d18091b208
Ark <A6611688@yandex.ru>
2024-10-17 22:09:56 +0300

    add: minimisation with tests, trying make tests work

---
+++ lab2/src/AutomatonMinimizer.hs
@@ -0,0 +1,84 @@
+module AutomatonMinimizer
+    ( Automaton(..)
+    , minimizeAutomaton
+    ) where
+
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Data.List (nub, (\\))
+import Data.Maybe (fromMaybe)
+
+-- Automaton data structure
+data Automaton = Automaton
+    { states :: [Int]
+    , alphabet :: [Char]
+    , transitions :: Map.Map (Int, Char) Int
+    , initialState :: Int
+    , acceptingStates :: [Int]
+    } deriving (Show, Eq)
+
+-- Main function to minimize the automaton
+minimizeAutomaton :: Automaton -> Automaton
+minimizeAutomaton aut =
+    let eqClasses = computeEquivalenceClasses aut
+        newStates = [0 .. (length eqClasses - 1)]
+        stateMap = Map.fromList [(s, findClassIndex s eqClasses) | s <- states aut]
+        newTransitions = Map.mapKeys (\(s, c) -> (stateMap Map.! s, c)) $
+                         Map.map (\s -> stateMap Map.! s) (transitions aut)
+        newInitialState = stateMap Map.! initialState aut
+        newAcceptingStates = nub [stateMap Map.! s | s <- acceptingStates aut]
+    in Automaton
+        { states = newStates
+        , alphabet = alphabet aut
+        , transitions = newTransitions
+        , initialState = newInitialState
+        , acceptingStates = newAcceptingStates
+        }
+
+-- Compute equivalence classes for minimization
+computeEquivalenceClasses :: Automaton -> [[Int]]
+computeEquivalenceClasses aut =
+    let initial = [acceptingStates aut, states aut \\ acceptingStates aut]
+    in refinePartition aut initial
+
+-- Refine partition until it stabilizes
+refinePartition :: Automaton -> [[Int]] -> [[Int]]
+refinePartition aut partition
+    | partition == refined = partition
+    | otherwise = refinePartition aut refined
+  where
+    refined = concatMap (refineClass aut partition) partition
+
+-- Refine a single equivalence class
+refineClass :: Automaton -> [[Int]] -> [Int] -> [[Int]]
+refineClass aut partition cls =
+    groupBy (\s1 s2 -> all (\a -> findClass (step aut s1 a) partition ==
+                                  findClass (step aut s2 a) partition)
+                           (alphabet aut))
+            cls
+
+-- Helper function to find the class of a state
+findClass :: Int -> [[Int]] -> Maybe Int
+findClass s = findIndex (elem s)
+
+-- Helper function to find the index of a class containing a state
+findClassIndex :: Int -> [[Int]] -> Int
+findClassIndex s classes = fromMaybe (-1) (findIndex (elem s) classes)
+
+-- Transition function
+step :: Automaton -> Int -> Char -> Int
+step aut s a = fromMaybe (-1) (Map.lookup (s, a) (transitions aut))
+
+-- Helper function: group elements by a predicate
+groupBy :: (a -> a -> Bool) -> [a] -> [[a]]
+groupBy _ [] = []
+groupBy eq (x:xs) = let (ys, zs) = span (eq x) xs in (x:ys) : groupBy eq zs
+
+-- Helper function: find index of first element satisfying a predicate
+findIndex :: (a -> Bool) -> [a] -> Maybe Int
+findIndex p = loop 0
+  where
+    loop _ [] = Nothing
+    loop i (x:xs)
+        | p x = Just i
+        | otherwise = loop (i+1) xs
---
+++ lab2/test/AutomatonMinimizerSpec.hs
@@ -0,0 +1,100 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module AutomatonMinimizerSpec (spec) where
+
+import Test.Hspec
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import AutomatonMinimizer
+
+-- Test suite for AutomatonMinimizer
+spec :: Spec
+spec = do
+  describe "minimizeAutomaton" $ do
+    it "minimizes a trivial DFA with one state" $ do
+      let dfa = Automaton {
+            states = [0],
+            alphabet = ['a', 'b'],
+            transitions = Map.fromList [],
+            initialState = 0,
+            acceptingStates = [0]
+          }
+          expected = dfa  -- The DFA is already minimal
+      minimizeAutomaton dfa `shouldBe` expected
+
+    it "minimizes a DFA with unreachable states" $ do
+      let dfa = Automaton {
+            states = [0, 1, 2],
+            alphabet = ['a', 'b'],
+            transitions = Map.fromList [((0, 'a'), 1), ((1, 'b'), 0)],  -- No transition to state 2
+            initialState = 0,
+            acceptingStates = [1]
+          }
+          expected = Automaton {
+            states = [0, 1],
+            alphabet = ['a', 'b'],
+            transitions = Map.fromList [((0, 'a'), 1), ((1, 'b'), 0)],
+            initialState = 0,
+            acceptingStates = [1]
+          }
+      minimizeAutomaton dfa `shouldBe` expected
+
+    it "minimizes a DFA with equivalent states" $ do
+      let dfa = Automaton {
+            states = [0, 1, 2, 3],
+            alphabet = ['a', 'b'],
+            transitions = Map.fromList [ ((0, 'a'), 1), ((0, 'b'), 2),
+                                         ((1, 'a'), 3), ((1, 'b'), 3),
+                                         ((2, 'a'), 3), ((2, 'b'), 3),
+                                         ((3, 'a'), 3), ((3, 'b'), 3) ],
+            initialState = 0,
+            acceptingStates = [3]
+          }
+          expected = Automaton {
+            states = [0, 1, 2],
+            alphabet = ['a', 'b'],
+            transitions = Map.fromList [ ((0, 'a'), 1), ((0, 'b'), 2),
+                                         ((1, 'a'), 2), ((1, 'b'), 2),
+                                         ((2, 'a'), 2), ((2, 'b'), 2) ],
+            initialState = 0,
+            acceptingStates = [2]
+          }
+      minimizeAutomaton dfa `shouldBe` expected
+
+    it "minimizes a DFA where no states are equivalent" $ do
+      let dfa = Automaton {
+            states = [0, 1, 2],
+            alphabet = ['a', 'b'],
+            transitions = Map.fromList [ ((0, 'a'), 1), ((0, 'b'), 2),
+                                         ((1, 'a'), 1), ((1, 'b'), 2),
+                                         ((2, 'a'), 1), ((2, 'b'), 2) ],
+            initialState = 0,
+            acceptingStates = [1]
+          }
+          expected = dfa  -- The DFA is already minimal
+      minimizeAutomaton dfa `shouldBe` expected
+
+    it "minimizes a complex DFA with multiple equivalent states" $ do
+      let dfa = Automaton {
+            states = [0, 1, 2, 3, 4, 5],
+            alphabet = ['a', 'b'],
+            transitions = Map.fromList [ ((0, 'a'), 1), ((0, 'b'), 2),
+                                         ((1, 'a'), 3), ((1, 'b'), 4),
+                                         ((2, 'a'), 3), ((2, 'b'), 4),
+                                         ((3, 'a'), 5), ((3, 'b'), 5),
+                                         ((4, 'a'), 5), ((4, 'b'), 5),
+                                         ((5, 'a'), 5), ((5, 'b'), 5) ],
+            initialState = 0,
+            acceptingStates = [5]
+          }
+          expected = Automaton {
+            states = [0, 1, 2, 3],
+            alphabet = ['a', 'b'],
+            transitions = Map.fromList [ ((0, 'a'), 1), ((0, 'b'), 2),
+                                         ((1, 'a'), 3), ((1, 'b'), 3),
+                                         ((2, 'a'), 3), ((2, 'b'), 3),
+                                         ((3, 'a'), 3), ((3, 'b'), 3) ],
+            initialState = 0,
+            acceptingStates = [3]
+          }
+      minimizeAutomaton dfa `shouldBe` expected

0b45b163a20d50c62cadbf39c34d8d0583f848e6
Ark <A6611688@yandex.ru>
2024-10-19 14:43:45 +0300

    upd: visualization

--- lab2/src/AutomatonMinimizer.hs
+++ lab2/src/AutomatonMinimizer.hs
@@ -90,16 +90,19 @@ minimizeAutomaton automaton =
 
 visualizeAutomaton :: Automaton -> IO ()
 visualizeAutomaton (Automaton states alph transitions initial accepting) = do
-    putStrLn "Automaton:"
-    putStrLn $ "States: " ++ show states
-    putStrLn $ "Alphabet: " ++ show alph
-    putStrLn $ "Initial state: " ++ show initial
-    putStrLn $ "Accepting states: " ++ show accepting
-    putStrLn "Transitions:"
-    putStrLn "digraph {"
-    mapM_ (\((from, char), to) -> putStrLn $ "    " ++ show from ++  " -> " ++ show to ++ " [label = \"" ++ [char] ++ "\"]") (Map.toList transitions)
---    mapM_ (\((from, char), to) -> putStrLn $ show from ++ " --" ++ [char] ++ "--> " ++ show to) (Map.toList transitions)
-    putStrLn "}"
+   putStrLn "Automaton:"
+   putStrLn $ "States: " ++ show states
+   putStrLn $ "Alphabet: " ++ show alph
+   putStrLn $ "Initial state: " ++ show initial
+   putStrLn $ "Accepting states: " ++ show accepting
+   putStrLn "Transitions:"
+   putStrLn "digraph {"
+   putStrLn $ "  \"start\" [shape=point];"
+   putStrLn $ "  \"start\" -> " ++ show initial ++ ";"
+   mapM_ (\x -> putStrLn $ "  " ++ show x ++ " [shape=doublecircle];") accepting
+   mapM_ (\((from, char), to) -> putStrLn $ "  " ++ show from ++ " -> " ++ show to ++ " [label = \"" ++ [char] ++ "\"]")
+         (Map.toList transitions)
+   putStrLn "}"
 
 
 

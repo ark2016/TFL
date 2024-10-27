051a9eccb6ad716f7cc81d29413bc7d8f1305053
HermanDp45 <HermannDp@edelkin.ru>
2024-10-27 20:44:37 +0300

    update python guesser v0.2 (Not full)

--- BaseGuesserForRelictMat/guesser.py
+++ BaseGuesserForRelictMat/guesser.py
@@ -3,43 +3,66 @@ from random import randint
 import requests
 import json
 
-# Класс для обращения к мату Реликта (Для тестирования базовой версии)
-class RelictMatQuerryModel:
-    def __init__(self, path_to_mat):
+# Класс для обращениѝ к мату Реликта (Для тестированиѝ базовой версии)
+class RelictMatQueryModel:
+    def __init__(self):
         self.result = "1"
         self.error_code = "! :-( !"
 
-    def membershipQuerry(self, word):
-        querry_body = {
+    def membershipQuery(self, word):
+        # word = word.replace("", "ε")
+        if word == "":
+            word = "ε"
+        query_body = {
             "word": word
         }
         try:
-            response = requests.get("/checkWorld", json.decoder(querry_body))
+            response = requests.post(
+                "http://127.0.0.1:8095/checkWord", 
+                data=json.dumps(query_body), 
+                headers={"Content-Type": "application/json"}
+            )
+            # print("HTTP Status Code:", response.status_code)
+            # print("Response Content:", response.content)
+            print(response.content)
+            response_json = response.json()
+            # print(response_json["response"])
+            return response_json["response"]
         except Exception as e:
-            print("Произошел error при проверке на включение :-( => ", e)
+            print("Произошла ошибка при проверке на включение :-( =>", e)
+            return None
 
-        return response.json()["response"]
-
-    def equivalenceQuerry(self, S, S_extra, E, table):
-        querry_body = {
+    def equivalenceQuery(self, S, S_extra, E, table):
+        query_body = {
             "main_prefixes": S,
             "non_main_prefixes": S_extra,
-            "suffixes": E, 
-            "table": table 
+            "suffixes": E,
+            "table": table
         }
+        # print(query_body)
         try:
-            response = requests.get("/checkTable", json.decoder(querry_body)).json()
-            if response["type"] and response["response"]:
-                    print("лабиринт МАТа имеет слова, которые не принадлежат языку пользователя, контрпример: ", response["response"])
-            elif response["type"] == False:
-                return (False, response["response"])
-            else:
-                return (True, "") 
-
+            response = requests.post(
+                "http://127.0.0.1:8095/checkTable", 
+                data=json.dumps(query_body), 
+                headers={"Content-Type": "application/json"}
+            )
+            print("HTTP Status Code:", response.status_code)
+            print("Response Content:", response.content)
+            
         except Exception as e:
-            print("Произошел error при проверке на эквивалентность :-( => ", e)
+            print("Произошла ошибка при проверке на эквивалентность :-( =>", e)
+            return None
+        print(response)
+        response = response.json()
+        # print(response)
+        if response["type"] and response["response"]:
+            return (False, response["response"])
+            print("лабиринт МАТа имеет слова, которые не принадлежат языку пользователѝ, контрпример:", response["response"])
+        elif response["type"] == False:
+            return (False, response["response"])
+        else:
+            return (True, "")
 
-        return response.json()["response"]
 
 # Class of SIMPLE L* Algorithm
 class LStarAlgorithm:
@@ -48,9 +71,9 @@ class LStarAlgorithm:
         self.A = A  # alphabet
         self.S = [""]  # префиксы, начинаются с ε
         self.E = [""]  # суффиксы, начинаются с ε
-        self.T = {}    # вся таблица (основная + расширенная часть)
+        self.T = {}    # вся таблица (основнаѝ + расширеннаѝ часть)
         
-        # инициализация таблицы
+        # инициализациѝ таблицы
         self.initialize_table()
 
     def initialize_table(self):
@@ -59,19 +82,19 @@ class LStarAlgorithm:
         # где f - результат {0,1} от membership query
         for s in self.S:
             for e in self.E:
-                self.T[(s, e)] = self.mat.membershipQuerry(s + e)
+                self.T[(s, e)] = self.mat.membershipQuery(s + e)
 
     # проверка на полноту
     def is_closed(self):
         # теория:
         # T - таблица
-        # неоюходимо проверить
+        # необходимо проверить
         # Если для всех строк s.a(s in S, a in A, s.a = s + a) 
         # существует s'(s' in S)  row(s') = row(s).a)
         for s1 in self.S:
             for a in self.A:
                 row_s1a = self.get_row(s1 + a)
-                # если ни одной
+                # еѝли ни одной
                 if not any(self.get_row(s) == row_s1a for s in self.S):
                     return False, s1, a
         return True, None, None
@@ -89,7 +112,7 @@ class LStarAlgorithm:
         return True, None, None, None, None
 
     def get_row(self, s):
-        # возращает строку из таблицы T
+        # возращает ѝтроку из таблицы T
         return tuple(self.T.get((s, e), '?') for e in self.E)
 
     def extend_table(self):  
@@ -97,15 +120,21 @@ class LStarAlgorithm:
             for a in self.A:
                 for e in self.E:
                     if (s + a, e) not in self.T:
-                        self.T[(s + a, e)] = self.mat.membershipQuerry(s + a + e)
+                        self.T[(s + a, e)] = self.mat.membershipQuery(s + a + e)
 
     def run(self):
-        while True:
+        # while True:
+        for i in range(10):
+            print("iteration i: ", i)
+            print(self.S)
+            print(self.E)
+            print(self.T)
+            print()
             # Step 1: проверка на полноту
             closed, s1, a = self.is_closed()
             if not closed:
                 # добавляем s1 ⋅ a в S 
-                self.S.add(s1 + a)
+                self.S.append(s1 + a)
                 self.extend_table()
                 continue
 
@@ -113,38 +142,70 @@ class LStarAlgorithm:
             consistent, s1, s2, a, e = self.is_consistent()
             if not consistent:
                 # добавляем a ⋅ e в E
-                self.E.add(a + e)
+                self.E.append(a + e)
                 self.extend_table()
                 continue
 
             # Шаг 3: equal query
             hypothesis = self.build_hypothesis()
-            equivalent, counterexample = self.mat.equivalence_query(hypothesis)
+            equivalent, counterexample = self.mat.equivalenceQuery(*hypothesis)
             if equivalent:
                 print("Алгоритм завершен. Модель правильна.")
                 return hypothesis
             else:
                 # adding conterexample and it's prefix to S
-                for i in range(len(counterexample) + 1):
-                    self.S.add(counterexample[:i])
+                for i in range(1, len(counterexample) + 1):
+                    self.S.append(counterexample[:i])
                 self.extend_table()
 
     def build_hypothesis(self):
-        non_main_prefixes = [x for x in self.T.keys() if not(x in self.S)]
+        non_main_prefixes = [x[0] for x in self.T.keys() if not(x[0] in self.S)]
         table_string = ""
         for s in self.S:
-            for e in self.E:
-                table_string += str(self.T.get((s, e)))
+            for e in self.E: 
+                if self.T.get((s, e)):
+                    table_string += "1 "
+                else:
+                    table_string += "0 "
         
         for s in non_main_prefixes:
             for e in self.E:
-                table_string += str(self.T.get((s, e)))
+                if self.T.get((s, e)):
+                    table_string += "1 "
+                else:
+                    table_string += "0 "
         
-        return self.mat.equivalenceQuerry(self.S, non_main_prefixes, self.E, self.T)
+        Sm = ""
+        for x in self.S:
+            if x == "": Sm += "ε "
+            else: Sm += str(x) + " "
+        Em = ""
+        for x in self.E:
+            if x == "": Em += "ε "
+            else: Sm += str(x) + " "
+        
+        Nm = ""
+        for x in non_main_prefixes:
+            if x == "": Nm += "ε "
+            else: Nm += str(x) + " "
+        
+        Sm = Sm[:len(Sm)-1]
+        Em = Em[:len(Em)-1]
+        Nm = Nm[:len(Nm)-1]
+        table_string = table_string[:len(table_string)-1]
+            
+        # print("___________________")
+        # print(Sm)
+        # print(Em)
+        # print(Nm)
+        # print(table_string)
+        # print("___________________")
+
+        return [Sm, Nm, Em, table_string]
 
 # A = ['a', 'b', 'c', '0', '1', '2']
 A = ["L", "R"]
-mat_model = RelictMatQuerryModel()
+mat_model = RelictMatQueryModel()
 l_star = LStarAlgorithm(A, mat_model)
 
 final_hypothesis = l_star.run()

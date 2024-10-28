d7ae82e5b00fe548f3d3a7feb32b11a6e20130d2
Ark <A6611688@yandex.ru>
2024-10-28 13:11:57 +0300

    upd: deleted some comments

--- lab2/src/Main.hs
+++ lab2/src/Main.hs
@@ -12,26 +12,16 @@ import System.IO (hFlush, stdout)
 -- Main function
 main :: IO ()
 main = do
---    putStrLn "Generating a random automaton..."
-    let automaton = generateRandomAutomaton 1  -- You can change the size parameter as needed
---    putStrLn "Automaton generated."
+    let automaton = generateRandomAutomaton 1
     mainLoop automaton
 
 -- Main loop to handle user interactions
 mainLoop :: Automaton -> IO ()
 mainLoop automaton = do
---    putStrLn "\nSelect an option:"
---    putStrLn "1. Check if a string is accepted by the automaton"
---    putStrLn "2. Check if another automaton (from equivalence table) is equivalent to the generated automaton"
---    putStrLn "3. Visualize the automaton"
---    putStrLn "4. Exit"
---    putStr "Enter your choice: "
     hFlush stdout
     option <- getLine
     case option of
         "1" -> do
---            inputStr <- getLine
---            isStringAccepted automaton inputStr
             checkStringInclusion automaton
             mainLoop automaton
         "2" -> do
@@ -48,21 +38,17 @@ mainLoop automaton = do
 -- Function to check if a string is accepted by the automaton
 checkStringInclusion :: Automaton -> IO ()
 checkStringInclusion automaton = do
---    putStr "Enter the string to check: "
     hFlush stdout
     inputStr <- getLine
     let result = isStringAccepted automaton inputStr
     if result == 1
         then putStrLn "1"
         else putStrLn "0"
---        then putStrLn "String is accepted by the automaton."
---        else putStrLn "String is NOT accepted by the automaton."
+
 
 -- Function to check if another automaton is equivalent to the generated one
 checkAutomatonEquivalence :: Automaton -> IO ()
 checkAutomatonEquivalence automaton = do
---    putStrLn "Enter the equivalence table as a list of (String, [(Char, String)], Int) tuples. For example: [(\"A\", [('a', \"B\")], 0), (\"B\", [('b', \"A\")], 1)]"
---    putStr "Equivalence table: "
     hFlush stdout
     eqTableInput <- getLine
     let maybeEqTable = reads eqTableInput :: [([(String, [(Char, String)], Int)], String)]
@@ -71,9 +57,6 @@ checkAutomatonEquivalence automaton = do
             let enteredAutomaton = fromEquivalenceTable eqTable
             let equivalenceResult = areAutomataEquivalent automaton enteredAutomaton
             case equivalenceResult of
---                Right True  -> putStrLn "The automata are equivalent."
                 Right True -> putStrLn "1"
---                Right False -> putStrLn "The automata are NOT equivalent."
                 Left msg -> putStrLn msg
---                Left errMsg -> putStrLn $ "Error during equivalence checking: " ++ errMsg
         _ -> putStrLn "Invalid input. Please enter a valid equivalence table."

362166e91f133e3bbf1b107d20f6f507ba1b5ef8
Ark <A6611688@yandex.ru>
2024-10-28 13:35:32 +0300

    upd: can read

--- lab2/src/Automaton.hs
+++ lab2/src/Automaton.hs
@@ -1,5 +1,8 @@
 module Automaton (Automaton(..)) where
 import qualified Data.Map as Map
+import Text.Read (Read(..), readPrec)
+import Text.ParserCombinators.ReadP (ReadP, string, readP_to_S, readS_to_P)
+import Control.Applicative ((<|>))
 -- | Модуль, предоставляющий структуру данных для автомата.
 {-|
 Тип данных 'Automaton' представляет конечный автомат.
@@ -30,3 +33,30 @@ data Automaton = Automaton
     , initialState :: Int
     , acceptingStates :: [Int]
     } deriving (Show, Eq)
+
+-- Реализация инстанса Read для Automaton
+instance Read Automaton where
+    readsPrec _ = readP_to_S parseAutomaton
+
+-- Парсер для автомата
+parseAutomaton :: ReadP Automaton
+parseAutomaton = do
+    _ <- string "Automaton {"
+    _ <- string "states = "
+    states <- readS_to_P reads
+    _ <- string ", alphabet = "
+    alphabet <- readS_to_P reads
+    _ <- string ", transitions = fromList "
+    transitions <- readS_to_P reads
+    _ <- string ", initialState = "
+    initialState <- readS_to_P reads
+    _ <- string ", acceptingStates = "
+    acceptingStates <- readS_to_P reads
+    _ <- string "}"
+    return Automaton
+        { states = states
+        , alphabet = alphabet
+        , transitions = Map.fromList transitions
+        , initialState = initialState
+        , acceptingStates = acceptingStates
+        }

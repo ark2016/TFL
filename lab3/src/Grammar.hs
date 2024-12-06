3911ec2cd66b9ccc4e7277a20ddc293b2cb04293
Ark <A6611688@yandex.ru>
2024-12-06 11:21:13 +0300

    upd: epsilon & Eq

--- lab3/src/Grammar.hs
+++ lab3/src/Grammar.hs
@@ -9,10 +9,12 @@ module Grammar (
 import Data.Char (isAlphaNum, isLower, isUpper)
 import Data.List (intercalate)
 import Text.Read (readMaybe)
+import Control.Monad (guard)
 
 -- Тип данных для символов грамматики
 data Symbol = Terminal Char
             | NonTerminal String
+            | Epsilon -- Добавляем символ для пустых правил
             deriving (Eq, Show)
 
 -- Тип данных для правил грамматики
@@ -22,11 +24,16 @@ type Rule = (Symbol, [Symbol])
 newtype Grammar = Grammar [Rule]
     deriving (Show)
 
+-- Добавляем инстанс Eq для Grammar
+instance Eq Grammar where
+  (Grammar g1) == (Grammar g2) = g1 == g2
+
 -- Функция для парсинга строки в символ
 parseSymbol :: String -> Maybe Symbol
 parseSymbol s
     | all isLower s && length s == 1 = Just (Terminal (head s))
     | all isAlphaNum s && isUpper (head s) = Just (NonTerminal s)
+    | s == "ε" = Just Epsilon -- Добавляем поддержку для epsilon
     | otherwise = Nothing
 
 -- Функция для парсинга строки в правило
@@ -34,11 +41,16 @@ parseRule :: String -> Maybe Rule
 parseRule s = do
     let parts = words s
     case parts of
-        [left, "->", right] -> do
-            let leftSymbol = NonTerminal left
-            rightSymbols <- mapM parseSymbol (words right)
+        (left: "->": right) -> do
+            leftSymbol <- parseNonTerminal left
+            rightSymbols <- mapM parseSymbol right
             return (leftSymbol, rightSymbols)
         _ -> Nothing
+
+parseNonTerminal :: String -> Maybe Symbol
+parseNonTerminal s
+  | all isAlphaNum s && isUpper (head s) = Just (NonTerminal s)
+  | otherwise = Nothing
 
 -- Функция для парсинга строки в грамматику
 parseGrammar :: String -> Maybe Grammar
@@ -51,7 +63,12 @@ parseGrammar s = do
 printGrammar :: Grammar -> String
 printGrammar (Grammar rules) = intercalate "\n" (map printRule rules)
     where
-        printRule (left, right) = show left ++ " -> " ++ intercalate " " (map show right)
+        printRule (left, right) = show left ++ " -> " ++ intercalate " " (map printSymbol right)
+
+        printSymbol :: Symbol -> String
+        printSymbol (Terminal c) = [c]
+        printSymbol (NonTerminal nt) = nt
+        printSymbol Epsilon = "ε"
 
 -- Пример использования
 main :: IO ()

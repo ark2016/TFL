2a03ca6b64b16a3bb509620c1bcd18cb7358bd98
Ark <A6611688@yandex.ru>
2025-01-15 00:40:49 +0300

    upd

--- lab4/src/Main.hs
+++ lab4/src/Main.hs
@@ -6,18 +6,21 @@ import Regex.Parser (parseRegex)
 import Regex.SyntaxChecker (checkRegex, CheckedRegex)
 import GrammarCFG.CFG (CFG(..), Production(..), Symbol(..), Nonterminal, Terminal)
 import GrammarCFG.GrammarBuilder (buildFrameGrammar)
+import GrammarCFG.AttributedGrammar (buildAttributedGrammar, AttributedCFG(..), AttributedProduction(..), Attrib(..))
 import Data.List (intercalate)
+import Control.Monad (when)
 
 main :: IO ()
 main = do
   -- Примеры входных регулярных выражений:
---  let input = "(a|b)(?2)(a(a(a(a(a(a(a(a(a(a))))))))))"
---  let input = "(a|(bb))(?2)"
---  let input = "((?:a(?2)|(bb))(?1))"
---  let input = "((?1)a|b)"
-  let input = "(?1)(a|b)*(?1)"
---  let input = "(aa(?1))"
---  let input = "(a(?1)a|b)"  -- простой пример (регекс для языка {a^n c b^n | n ∈ N})
+  -- let input = "(a|b)(?2)(a(a(a(a(a(a(a(a(a(a))))))))))"
+  -- let input = "(a|(bb))(?2)"
+  -- let input = "((?:a(?2)|(bb))(?1))"
+  -- let input = "((?1)a|b)"
+  --let input = "(?1)(a|b)*(?1)"
+  -- let input = "(aa(?1))"
+  -- let input = "(a(?1)a|b)"  -- простой пример (регекс для языка {a^n c b^n | n ∈ N})
+  let input = "((?=a)(a|b))"
   putStrLn $ "Input regex: " ++ input
 
   -- Парсинг
@@ -34,9 +37,14 @@ main = do
           putStrLn $ "Checked AST: " ++ show goodAst
 
           -- Построение КС-грамматики
-          let cfg = buildFrameGrammar ast --goodAst
+          let cfg = buildFrameGrammar goodAst
           putStrLn "Generated CFG:"
           printCFG cfg
+
+          -- Построение атрибутной КС-грамматики
+          let attCfg = buildAttributedGrammar goodAst
+          putStrLn "Generated Attributed CFG:"
+          printAttributedCFG attCfg
 
 --------------------------------------------------------------------------------
 -- | Функция для отображения КС-грамматики в удобочитаемом виде
@@ -54,7 +62,33 @@ printCFG (CFG nonterms terms start prods) = do
         (if null rhs
            then "Epsilon"
            else intercalate " " (map showSymbol rhs))
-    
+
     showSymbol :: Symbol -> String
     showSymbol (N nt) = nt
     showSymbol (T c)  = "'" ++ [c] ++ "'"
+
+--------------------------------------------------------------------------------
+-- | Функция для отображения атрибутной КС-грамматики в удобочитаемом виде
+printAttributedCFG :: AttributedCFG -> IO ()
+printAttributedCFG (AttributedCFG nts ts start prods) = do
+  putStrLn $ "  Start symbol: " ++ start
+  putStrLn $ "  Nonterminals: " ++ show nts
+  putStrLn $ "  Terminals: " ++ show ts
+  putStrLn "  Attributed Productions:"
+  mapM_ printAttProd prods
+  where
+    printAttProd :: AttributedProduction -> IO ()
+    printAttProd (AttributedProduction p inh syn) =
+      putStrLn $ "    " ++ lhs p ++ " -> " ++ rhsStr p ++
+                 " | Inherited: " ++ show inh ++
+                 " | Synthesized: " ++ show syn
+      where
+        rhsStr :: Production -> String
+        rhsStr (Production _ rhs) =
+          if null rhs
+            then "Epsilon"
+            else intercalate " " (map showSymbol rhs)
+
+        showSymbol :: Symbol -> String
+        showSymbol (N nt) = nt
+        showSymbol (T c)  = "'" ++ [c] ++ "'"
